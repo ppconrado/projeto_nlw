2a4ad958645f5e251de3ed98c21b1ea63fe76777
ppconrado <ppconrado@yahoo.com.br>
2021-04-20 03:48:31 -0300

    Project

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { response } from "express";
+
+const app = express();
+
+/**
+ * GET    -> Buscas
+ * POST   -> Criação
+ * PUT    -> Alteração
+ * DELETE -> Deletar
+ * PATCH  -> Alterar 
